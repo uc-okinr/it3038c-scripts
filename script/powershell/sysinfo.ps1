516966e075783d6eff97d83a24ac698ea0b2d77b
uc-okinr <okinr@mail.uc.edu>
2021-02-02 23:21:18 -0500

    New linux files

---
+++ script/powershell/sysinfo.ps1
@@ -0,0 +1,10 @@
+﻿function getIP{
+    (Get-NetIPAddress).ipv4IPAddress | 
+Select-String "192*"
+}
+write-host (getIP)
+
+$ip = getIP
+Write-Host("This machine's IP is $ip")
+Write-Host("This machine's IP is {0}")
+-f $ip

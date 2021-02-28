76a83eb85c73185671c58a97525ba80027a013b2
uc-okinr <okinr@mail.uc.edu>
2021-02-28 18:37:52 -0500

    adding Json files

---
+++ node/server.js
@@ -0,0 +1,59 @@
+
+var http = require("http");
+var fs = require("fs");
+var os = require("os");
+var ip = require('ip');
+const { cpuUsage } = require("process");
+const { time } = require("console");
+
+
+http.createServer(function(req, res){
+
+    if (req.url === "/") {
+        fs.readFile("./public/index.html", "UTF-8", function(err, body){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(body);
+    });
+}
+    else if(req.url.match("/sysinfo")) {
+        myHostName=os.hostname();
+        myfreemem=os.freemem();
+        
+        var ut_sec = os.uptime(); 
+var ut_min = ut_sec/60; 
+var ut_hour = ut_min/60; 
+   
+ut_sec = Math.floor(ut_sec); 
+ut_min = Math.floor(ut_min); 
+ut_hour = Math.floor(ut_hour); 
+  
+ut_hour = ut_hour%60; 
+ut_min = ut_min%60; 
+ut_sec = ut_sec%60; 
+  
+        
+        html=`    
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Node JS Response</title>
+          </head>
+          <body>
+            <p>Hostname: ${myHostName}</p>
+            <p>IP: ${ip.address()}</p>
+            <p>Server Uptime: ${ut_hour} Hours ${ut_min} mins ${ut_sec} seconds </p>
+            <p>Total Memory: ${os.totalmem /1024/ 1024 }MB  </p>
+            <p>Free Memory: ${myfreemem / 1024 /1024 }MB </p>
+            <p>Number of CPUs: ${os.cpus().length} </p>            
+          </body>
+        </html>` 
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(html);
+    }
+    else {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end(`404 File Not Found at ${req.url}`);
+    }
+}).listen(3000);
+
+console.log("Server listening on port 3000");
